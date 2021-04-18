8d7f7d56d3912293d564d6f83af74c06ddb4cdcd
Hansol Yun <hsyoon315@gmail.com>
2021-04-19 00:59:42 +0900

    Add: initial authorization in users controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,3 +1,16 @@
-exports.login = async (req, res, next) => {
+const { OAuth2Client } = require("google-auth-library");
 
+exports.login = async (req, res, next) => {
+  try {
+    const googleToken = req.headers.authorization.split(" ")[1];
+    const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+    const ticket = await client.verifyIdToken({
+      idToken: googleToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const { name, email, picture } = ticket.getPayload();
+  } catch (err) {
+    next(err);
+  }
 };
