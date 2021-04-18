7b8d87429b1c98a59685cffc793a78aed91ee635
Jeongbeen <pjb6510@gmail.com>
2021-04-18 12:31:21 +0900

    Style: change line break

--- config/connectMongoDB.js
+++ config/connectMongoDB.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectMongoDB = () => {
-  mongoose.connect(process.env.MONGO_DB,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      dbName: "fantasy-baseball",
-    });
+  mongoose.connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    dbName: "fantasy-baseball",
+  });
 
   const db = mongoose.connection;
   db.on("error", (err) => console.error(`DB connection Error : \n${err}`));
