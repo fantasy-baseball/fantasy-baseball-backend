0444e87e9b0f8f091e4a0b5ebbe5f417d21985dc
Jeongbeen <pjb6510@gmail.com>
2021-04-18 12:32:24 +0900

    Add: schedule schema in Game schema

--- models/Game.js
+++ models/Game.js
@@ -3,14 +3,41 @@ const mongoose = require("mongoose");
 const gameSchema = new mongoose.Schema({
   date: {
     type: Date,
-    require: true,
+    required: true,
   },
   user_play_data: [{ type: mongoose.Schema.Types.ObjectId }],
   players: [{ type: mongoose.Schema.Types.ObjectId }],
-  schedule: Array,
+  schedule: [{
+    id: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    stadium: {
+      type: String,
+      required: true,
+    },
+    home: {
+      type: String,
+      required: true,
+    },
+    away: {
+      type: String,
+      required: true,
+    },
+    homePitcher: String,
+    awayPitcher: String,
+  }],
   total_money: Number,
   total_score: Number,
-  is_opend: Boolean,
+  is_opened: Boolean,
 });
 
 module.exports = mongoose.model("Game", gameSchema);
