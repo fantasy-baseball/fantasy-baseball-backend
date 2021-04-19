5c59c80d1973defb43ec1cf191321fc0e1f2ef56
Hansol Yun <hsyoon315@gmail.com>
2021-04-19 15:29:36 +0900

    Add: authenticateUser middleware in users login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const usersController = require("../controllers/users.controller");
+const { authenticateUser } = require("../middlewares/authorization");
 
 const router = express.Router();
 
-router.get("/login", usersController.login);
+router.get("/login", authenticateUser, usersController.login);
 router.get("/logout", usersController.logout);
 
 module.exports = router;
