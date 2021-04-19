37013f53222af0c22168e5da5743988add7d157d
Hansol Yun <hsyoon315@gmail.com>
2021-04-19 15:27:17 +0900

    Refactor: authenticateUser

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,12 +1,24 @@
-const jwt = require("jsonwebtoken");
+const { OAuth2Client } = require("google-auth-library");
 
-const authenticateUser = (req, res, next) => {
+const authenticateUser = async (req, res, next) => {
+  const googleToken = req.headers.authorization.split(" ")[1];
+  const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
   try {
-    const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.SECRET_KEY);
+    const ticket = await client.verifyIdToken({
+      idToken: googleToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+    const { name, email, picture } = ticket.getPayload();
+
+    res.locals.googleToken = googleToken;
+    res.locals.profile = { name, email, picture };
     next();
   } catch (err) {
-    next(err);
+    res.status(401).json({
+      result: "failure",
+      message: "Unauthorized",
+    });
   }
 };
 
