861052a207fee1d26ebf43198f62894fcb2a8fc0
Jeongbeen <pjb6510@gmail.com>
2021-04-20 19:10:54 +0900

    Add: getPlayers in games

--- controllers/games.controller.js
+++ controllers/games.controller.js
@@ -1,5 +1,6 @@
 const createError = require("http-errors");
 const Game = require("../models/Game");
+require("../models/Player");
 
 exports.getSchedule = async (req, res, next) => {
   try {
@@ -17,6 +18,30 @@ exports.getSchedule = async (req, res, next) => {
     res.status(200).json({
       result: "ok",
       data: game.schedule,
+    });
+  } catch (err) {
+    next(createError(500, err.message));
+  }
+};
+
+exports.getPlayers = async (req, res, next) => {
+  try {
+    const gameDate = req.params.game_date;
+    const game = await Game.findOne({ gameDate })
+      .populate("players")
+      .lean();
+
+    if (!game) {
+      next(createError(
+        404,
+        "Can't find Game that corresponds to the game_date"
+      ));
+      return;
+    }
+
+    res.status(200).json({
+      result: "ok",
+      data: game.players,
     });
   } catch (err) {
     next(createError(500, err.message));
--- routes/games.js
+++ routes/games.js
@@ -4,5 +4,6 @@ const gamesController = require("../controllers/games.controller");
 const router = express.Router();
 
 router.get("/:game_date/schedule", gamesController.getSchedule);
+router.get("/:game_date/players", gamesController.getPlayers);
 
 module.exports = router;
