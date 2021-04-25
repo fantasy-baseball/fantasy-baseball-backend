d67bb389cad8a1cb65a8d6f628509d622a338008
eluka22 <eluka22@gmail.com>
2021-04-26 01:19:26 +0900

    Add: PLAYER_POSITION in constants/game

--- constants/game.js
+++ constants/game.js
@@ -9,3 +9,16 @@ module.exports.BETTING_START_TIME = {
   saturday: "15:00:00",
   sunday: "12:00:00",
 };
+
+module.exports.PLAYER_POSITION = {
+  좌익수: "leftFielder",
+  중견수: "centerFielder",
+  우익수: "rightFielder",
+  "1루수": "firstBaseman",
+  "2루수": "secondBaseman",
+  "3루수": "thirdBaseman",
+  유격수: "shortStop",
+  투수: "pitcher",
+  포수: "catcher",
+  지명타자: "designatedHitter",
+};
