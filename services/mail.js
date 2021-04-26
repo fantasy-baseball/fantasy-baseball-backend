78e3f7295fc847e0455f59684fd63b9528355b7f
Hansol Yun <hsyoon315@gmail.com>
2021-04-26 16:28:38 +0900

    Add: fetch user email, html template in mail

--- services/mail.js
+++ services/mail.js
@@ -1,20 +1,34 @@
+const fs = require("fs");
+const { promisify } = require("util");
+const path = require("path");
 const nodemailer = require("nodemailer");
 const { OAuth2Client } = require("google-auth-library");
+const User = require("../models/User");
 
-const mails = [];
-// TODO: DB에서 유저 이메일 가져오기
 const {
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
   REDIRECT_URL,
   REFRESH_TOKEN,
+  MANAGER_EMAIL,
 } = process.env;
 
 const oauth = new OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, REDIRECT_URL);
 oauth.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-const sendBettingStartMail = async () => {
+const readFile = promisify(fs.readFile);
+const templatePath = path.join(__dirname, "../public/email.html");
+const mails = [];
+
+const sendGrouptMail = async () => {
   try {
+    // const users = await User.find().lean();
+
+    // for (let i = 0; i < users.length; i += 1) {
+    //   mails.push(users[i].email);
+    // }
+    // 유저 이메일 가져오기
+
     const accessToken = await oauth.getAccessToken();
 
     const transport = nodemailer.createTransport({
@@ -24,7 +38,7 @@ const sendBettingStartMail = async () => {
       service: "gamil",
       auth: {
         type: "OAuth2",
-        user: "solhanyun@gmail.com",
+        user: MANAGER_EMAIL,
         clientId: GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
         refreshToken: REFRESH_TOKEN,
@@ -33,10 +47,10 @@ const sendBettingStartMail = async () => {
     });
 
     const mailOptions = {
-      from: "FANTASY BASEBALL <solhanyun@gmail.com>",
-      to: mails,
+      from: `FANTASY BASEBALL <${MANAGER_EMAIL}>`,
+      bcc: mails,
       subject: "FANTASY BASEBALL",
-      text: "베팅이 시작되었습니다",
+      html: await readFile(templatePath, "utf-8"),
     };
 
     const result = await transport.sendMail(mailOptions);
@@ -46,4 +60,4 @@ const sendBettingStartMail = async () => {
   }
 };
 
-module.exports = sendBettingStartMail;
+module.exports = sendGrouptMail;
