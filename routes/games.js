c4b383f36d34200fc3256996a5e8f0467b2a6189
Jeongbeen <pjb6510@gmail.com>
2021-04-27 03:18:23 +0900

    Add: getBettingHistory

--- routes/games.js
+++ routes/games.js
@@ -7,10 +7,11 @@ const router = express.Router();
 
 router.get("/:game_date/schedule", gamesController.getSchedule);
 router.get("/:game_date/players", gamesController.getPlayers);
-router.get("/:game_date/betting", gamesController.getBettingData);
+router.get("/:game_date/betting", gamesController.getBettingStatus);
 router.post("/:game_date/betting", authenticateUser, gamesController.postBetting);
 
 router.get("/:game_date/rankings/users", historyController.getUserRankings);
 router.get("/:game_date/roaster", authenticateUser, historyController.getRoaster);
+router.get("/betting-history", authenticateUser, historyController.getBettingHistory);
 
 module.exports = router;
