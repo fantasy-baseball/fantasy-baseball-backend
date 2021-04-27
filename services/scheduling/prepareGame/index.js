e6040617defb68d9b38f5cbe6eb23c01c5136c82
Jeongbeen <pjb6510@gmail.com>
2021-04-27 23:36:34 +0900

    Refactor: add fetchGameScheduleAndSave

--- services/scheduling/prepareGame/index.js
+++ services/scheduling/prepareGame/index.js
@@ -1,7 +1,5 @@
 const { format, subDays } = require("date-fns");
-const Game = require("../../../models/Game");
-
-const fetchGameSchedule = require("../../fetchGameInfoFromKBO/fetchGameSchedule");
+const fetchGameScheduleAndSave = require("./fetchGameScheduleAndSave");
 const {
   calculateBettingMoney,
   sumEarnedMoneyWithUserMoney,
@@ -11,28 +9,17 @@ const {
 module.exports = async () => {
   try {
     const dateString = format(new Date(), "yyyyMMdd");
-    console.log(dateString, "Preparation start");
-
     const yesterdayDateString = format(
       subDays(new Date(), 1),
       "yyyyMMdd"
     );
 
-    const gameList = await fetchGameSchedule(dateString);
-    console.log(`${dateString} Today game schedule crawling`);
-
-    await Game.create({
-      gameDate: dateString,
-      schedule: gameList,
-      createdAt: new Date(),
-    });
-
-    console.log(`${dateString} Game document is created`);
+    await fetchGameScheduleAndSave(dateString);
 
     await calculateBettingMoney(yesterdayDateString);
     await sumEarnedMoneyWithUserMoney(yesterdayDateString);
     await setBettingRankings(yesterdayDateString);
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
