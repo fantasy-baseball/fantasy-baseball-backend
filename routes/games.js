0e77d8abb332cf953ca75ecd8487d0b6c887c2d3
Jeongbeen <pjb6510@gmail.com>
2021-04-28 12:13:25 +0900

    Remove: history controller

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const gamesController = require("../controllers/games.controller");
-const historyController = require("../controllers/history.controller");
 const authenticateUser = require("../middlewares/authenticateUser");
 
 const router = express.Router();
@@ -9,11 +8,10 @@ router.get("/:game_date/schedule", gamesController.getSchedule);
 router.get("/:game_date/players", gamesController.getPlayers);
 router.get("/:game_date/betting", gamesController.getBettingStatus);
 router.post("/:game_date/betting", authenticateUser, gamesController.postBetting);
-
-router.get("/:game_date/rankings/users", historyController.getUserRankings);
-router.get("/:game_date/rankings/positions", historyController.getPositionRankings);
-router.get("/:game_date/rankings/players", historyController.getPlayerRankings);
-router.get("/:game_date/roaster", authenticateUser, historyController.getRoaster);
-router.get("/betting-history", authenticateUser, historyController.getBettingHistory);
+router.get("/:game_date/rankings/users", gamesController.getUserRankings);
+router.get("/:game_date/rankings/positions", gamesController.getPositionRankings);
+router.get("/:game_date/rankings/players", gamesController.getPlayerRankings);
+router.get("/:game_date/roaster", authenticateUser, gamesController.getRoaster);
+router.get("/betting-history", authenticateUser, gamesController.getBettingHistory);
 
 module.exports = router;
