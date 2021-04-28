06cc551bf527fd0563ea8fa37898e6924b8da81d
Jeongbeen <pjb6510@gmail.com>
2021-04-28 10:02:47 +0900

    Add: fetchGameResultAndSave in prepareGame

--- services/scheduling/prepareGame/index.js
+++ services/scheduling/prepareGame/index.js
@@ -1,10 +1,14 @@
 const { format, subDays } = require("date-fns");
 const fetchGameScheduleAndSave = require("./fetchGameScheduleAndSave");
+const fetchGameResultAndSave = require("./fetchGameResultAndSave");
 const {
   calculateBettingMoney,
   sumEarnedMoneyWithUserMoney,
   setBettingRankings,
 } = require("../../../utils/calculateBettingMoney");
+const updateHitterScore = require("../../../utils/updateHitterScore");
+const updatePitcherScore = require("../../../utils/updatePitcherScore");
+const updateTotalScore = require("../../../utils/updateTotalScore");
 
 module.exports = async () => {
   try {
@@ -15,10 +19,18 @@ module.exports = async () => {
     );
 
     await fetchGameScheduleAndSave(dateString);
+    await fetchGameResultAndSave(yesterdayDateString);
+    console.log("fetch today schedule, yesterday result");
+
+    await updateHitterScore(yesterdayDateString);
+    await updatePitcherScore(yesterdayDateString);
+    await updateTotalScore(yesterdayDateString);
+    console.log("update score");
 
     await calculateBettingMoney(yesterdayDateString);
     await sumEarnedMoneyWithUserMoney(yesterdayDateString);
     await setBettingRankings(yesterdayDateString);
+    console.log("update money");
   } catch (err) {
     console.error(err);
   }
