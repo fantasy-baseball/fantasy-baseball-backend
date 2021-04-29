b8112e045a04ad3a59f7600a78ca7a567c04513f
eluka22 <eluka22@gmail.com>
2021-04-29 20:23:16 +0900

    Test: change schedule time

--- loader/registerSchedule/scheduleTimes.js
+++ loader/registerSchedule/scheduleTimes.js
@@ -11,7 +11,7 @@ exports.preparationTime = preparationTime;
 const weekdayGameOpenTime = new schedule.RecurrenceRule();
 weekdayGameOpenTime.dayOfWeek = [new schedule.Range(2, 5)];
 weekdayGameOpenTime.hour = 20;
-weekdayGameOpenTime.minute = 10;
+weekdayGameOpenTime.minute = 30;
 weekdayGameOpenTime.tz = "Asia/Seoul";
 
 exports.weekdayGameOpenTime = weekdayGameOpenTime;
