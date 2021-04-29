f34251ff3ead728310bcad94f3a41ca48990fd45
Jeongbeen <pjb6510@gmail.com>
2021-04-29 19:39:37 +0900

    Test: change game open time

--- loader/registerSchedule/scheduleTimes.js
+++ loader/registerSchedule/scheduleTimes.js
@@ -10,8 +10,8 @@ exports.preparationTime = preparationTime;
 
 const weekdayGameOpenTime = new schedule.RecurrenceRule();
 weekdayGameOpenTime.dayOfWeek = [new schedule.Range(2, 5)];
-weekdayGameOpenTime.hour = 18;
-weekdayGameOpenTime.minute = 30;
+weekdayGameOpenTime.hour = 20;
+weekdayGameOpenTime.minute = 10;
 weekdayGameOpenTime.tz = "Asia/Seoul";
 
 exports.weekdayGameOpenTime = weekdayGameOpenTime;
